ba865348626302855f2cfdc194fe6992659b2391
Hao <haodev2000@gmail.com>
2021-09-17 17:17:32 +0700

    Bai tap thuc hanh tuan 2

---
+++ app/src/main/java/com/example/bt_tuan_02/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bt_tuan_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
